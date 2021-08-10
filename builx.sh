db325dda40770eace8b56c56e18dc14d77817014
mc303 <daniel@buonocore.nl>
2021-08-10 12:10:02 +0200

    Update builx.sh

--- builx.sh
+++ builx.sh
@@ -2,13 +2,15 @@
 #env DOCKER_BUILDKIT=1 docker build --no-cache -t mc303/samba .
 env DOCKER_BUILDKIT=1
 
+BUILDENV=buildsamba
+
 #create platform buildx env
-docker buildx create --platform linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 --name buildsamba --driver docker-container
-docker buildx use buildsamba
+docker buildx create --platform linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 --name $BUILDENV --driver docker-container
+docker buildx use $BUILDENV
 
 # build platforms
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t ghcr.io/mc303/samba --push .
 #docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t ghcr.io/mc303/samba  .
 
 # remove build env
-docker buildx rm buildsamba
+docker buildx rm $BUILDENV
