9e080dd457d0c20ec8601433518b70909f8f81f0
mc303 <daniel@buonocore.nl>
2021-08-10 12:30:55 +0200

    Update builx.sh

--- builx.sh
+++ builx.sh
@@ -4,6 +4,8 @@ env DOCKER_BUILDKIT=1
 
 BUILDENV=buildsamba
 
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
 #create platform buildx env
 docker buildx create --platform linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 --name $BUILDENV --driver docker-container
 docker buildx use $BUILDENV
